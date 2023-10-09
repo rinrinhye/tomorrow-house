5df4db286ac5890e441ef32a250a07d6f0b0fe1c
rinrinhye <115080424+rinrinhye@users.noreply.github.com>
2023-10-10 00:27:33 +0900

    🚧 Initiate a new carousel : .user-gallery

---
+++ js/Carousels.js
@@ -0,0 +1,31 @@
+const productCarousel = tns({
+  container: '.product-carousel .slider-list',
+  items: 1,
+  controls: false,
+  navContainer: '.product-carousel .thumbnail-list',
+  navAsThumbnails: true,
+  arrowKeys: true,
+  autoplay: true,
+  autoplayHoverPause: true,
+  autoplayButtonOutput: false,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+})
+
+const userGallery = tns({
+  container: '.user-gallery .slider-list',
+  controls: false,
+  gutter: 4,
+  edgePadding: 16,
+  loop: false,
+  arrowKeys: true,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+  responsive: {
+    768: {
+      gutter: 6,
+      controls: true,
+      edgePadding: 75,
+    },
+  },
+})
